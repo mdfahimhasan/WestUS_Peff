536c482e3316d260116fe4d04033898e7d19f594
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2025-02-26 13:13:23 -0700

    deleted scratch script

--- Codes/AZ/scratch.py
+++
@@ -1,4 +0,0 @@
-from Codes.utils.raster_ops import create_ref_raster
-
-create_ref_raster(input_raster=r'F:\WestUS_NetGW\scratch\GW_2023.tif',
-                  output_ref_raster=r'F:\WestUS_NetGW\scratch\AZ_gee_merge_ref_raster_30m.tif')

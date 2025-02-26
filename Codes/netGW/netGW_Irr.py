0c08205426b9f507081290eda1d10f7efb04080f
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2025-02-26 09:08:08 -0700

    comment

--- Codes/netGW/netGW_Irr.py
+++ Codes/netGW/netGW_Irr.py
@@ -85,7 +85,7 @@ def estimate_netGW_Irr(years_list, effective_precip_dir_pp, irrigated_cropET_dir
 
 
 if __name__ == '__main__':
-    # estimating netGW (coverage WestUS, need to refine by adding SW irrigation)
+    # estimating netGW (coverage WestUS)
     years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
              2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
     model_version = 'v19'
